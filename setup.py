e704279d83e419443e8c062c37949daa04d3d854
Adrian <adi.moto.woj@gmail.com>
2019-06-06 11:46:47 +0200

    v1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dpp9-0.1',
+    version='0.1',
+    packages=['myBib'],
+    license='MIT',
+    description='Moja biblioteka',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/Adi125r/dpp9',
+    author='Adrian Wojcieszek',
+    author_email='236055@student.pwr.edu.pl'
+)
